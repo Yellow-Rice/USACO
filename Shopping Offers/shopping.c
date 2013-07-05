2c1e2d0bf65950b1ed7ba3185d27f041c62f3333
Yellow-Rice <philipyyh@gmail.com>
2013-07-06 05:12:54 +0800

    USACO commit

--- Shopping Offers/shopping.c
+++ Shopping Offers/shopping.c
@@ -66,9 +66,9 @@ int main()
         price[product[i][0]] = product[i][2];
     }
     fclose(fin);
-    
+
     search(0, 0);
-    
+
     FILE *fout = fopen("shopping.out", "w");
     fprintf(fout, "%d\n", min);
     fclose(fout);
